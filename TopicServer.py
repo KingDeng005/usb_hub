73583ff948327280218551afb2fa9b9b648a1824
KingDeng005 <fuheng.deng@tusimple.ai>
2017-05-26 16:04:15 -0700

    add more sanity check to server; implement find_hub_and_port, and usb_hub_init: only start usb when there is a new messge coming in.

--- TopicServer.py
+++ TopicServer.py
@@ -7,50 +7,79 @@ import brainstem
 import numpy as np
 import time
 
-spec = brainstem.discover.findFirstModule(brainstem.link.Spec.USB)
-stem = brainstem.stem.USBStem()
-stem.connect(0xD80C7D63)
-usb = brainstem.stem.USB(stem, 0)
+# serial number mapped to port number - 2 usb hubs
+hub1_camera = ["0x00000001",\
+	      	"0x00000002",\
+	      	"16369133",\
+              	"0x00000004",\
+              	"0x00000005",\
+              	"0x00000006",\
+              	"0x00000007",\
+	      	"0x00000008"]
+hub2_camera = ["0x00000001",\
+	      	"0x00000002",\
+	      	"16369133",\
+              	"0x00000004",\
+              	"0x00000005",\
+              	"0x00000006",\
+              	"0x00000007",\
+	      	"0x00000008"]
+hub_camera = []
+hub_camera.append(hub1_camera)
+hub_camera.append(hub1_camera)
+usb_hub_list = [0xD80C7D63, 0x0000000]  # decide later
 
-# get each channel (0-7) state  
-index_0 = 1
-index_8 = 0x100
-index_16 = 0x10000
-index_24 = 0x1000000
+def find_hub_and_port(camera_serial):
+	err_cnt = 0
+	for i in range(len(usb_hub_list)):
+		try:
+			port = hub_camera[i].index(camera_serial)
+			return [i, port]
+		except:
+			continue
+	return -1
 
-# serial number mapped to port number
-camera_num = ["0x00000001",\
-	      "0x00000002",\
-	      "16369133",\
-              "0x00000004",\
-              "0x00000005",\
-              "0x00000006",\
-              "0x00000007",\
-	      "0x00000008"]
-
-# mask sequence for hub state
-a = np.array([index_0, index_8, index_16, index_24])
-index = np.vstack([a,a])
-b = np.arange(4)
-channel = np.vstack([b, b+4]) 
+def usb_hub_init(hub_serial):
+	# initialize brainstem module and connect to the device
+	spec = brainstem.discover.findFirstModule(brainstem.link.Spec.USB)
+	stem = brainstem.stem.USBStem()
+	res = stem.connect(hub_serial)
+	if res is not 0:
+		rospy.logerror("hub connection failed...")
+	usb = brainstem.stem.USB(stem, 0)
+	return usb
 
 def err_handling(req):
-	# locate which port
-	port = camera_num.index(req.serial_num)
-	usb.setPortDisable(port)	
-	a = usb.setPortEnable(port)
+	index = find_hub_and_port(req.serial_num)
+	if not isinstance(index, list):
+		rospy.logerr('the serial number doesn\'t exist')
+		res = req.serial_num + " 1"
+		return err_stringResponse(res)
+	# initialize the usb hub and get the usb instance
+	usb = usb_hub_init(index[0])
+	usb.setPortDisable(index[1])	
+	a = usb.setPortEnable(index[1])
+	try_num = 5   # try 5 times to ensure enabling
+	while a is not 0 and try_num > 0:
+		rospy.logwarning('usb_hub node: unable to enable the port...try again...')
+		a = usb.setPortEnable(port)
+		time.sleep(0.2)
+		try_num -= 1
+	# see the enabling result
 	if a is 0:
-		res = "Device(serial number:%s) has been successfully enabled"%req.serial_num
+		res_msg = "Device(serial number:%s) has been successfully enabled"%req.serial_num
+		rospy.loginfo(res_msg) 
+		res = req.serial_num + " 0"   # example return: 16369133 0 - enabling success	
 		return err_stringResponse(res)
 	else:	
-		res = "Device(serial number:%s) enabling failed"%req.serial_num
+		res_msg = "Device(serial number:%s) enabling failed"%req.serial_num
+		rospy.logerr(res_msg)
+		res = req.serial_num + " 1"  # example return: 16369133 1 - enabling fail
 		return err_stringResponse(res)
 
 def start_server():
-	print 'here'
-	rospy.init_node('usb_hub')
+	rospy.init_node('TopicServer')
 	s = rospy.Service('usb_hub_server', err_string, err_handling)
 	rospy.spin()
-
-a = usb.setPortEnable(2)
-start_server()
+if __name__ == "__main__":
+	start_server()
