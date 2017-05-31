e4e3937d083be96f7678d9139aea7b35b69053f0
KingDeng005 <fuheng.deng@tusimple.ai>
2017-05-31 16:23:49 -0700

    functional v.2 - server and usb hub encapsulation done

--- USB_HUB.py
+++ USB_HUB.py
@@ -1,24 +1,53 @@
+#!/usr/bin/env python
 '''
 THIS CLASS IS AN ENCAPSULATION OF USB HUB
+CREATED BY FUHENG DENG 5/31/2017
 '''
 
 import numpy as np
 import brainstem as bs
 import rospy
 
-class usb_hub(Object):
+# serial number mapped to port number - 2 usb hubs
+hub0_device = ["16398896",\
+	      	   "16456113",\
+	      	   "16455921",\
+               "16456119",\
+               "16456117",\
+               "0x0000000",\
+               "0x0000000",\
+	      	   "0x0000000"]
+hub1_device = ["16456116",\
+	      	   "16456118",\
+	      	   "16455958",\
+               "16455959",\
+               "16456114",\
+               "0x00000006",\
+               "0x00000007",\
+	      	   "0x00000008"]
 
-	def __init__(self, module_index=0):
-		self.modules = [0x78ADDCC2, 0xD80C7D63]
+class usb_hub:
+
+	def __init__(self):
+		self.modules = [0xD80C7D63,0x78ADDCC2]
+		self.device_list = []
+		self.device_list.append(hub0_device)  # camera 1 - 5
+		self.device_list.append(hub1_device)  # camera 6 - 10
 		self.stem = bs.stem.USBStem()
-		self.stem.connect(module_index)
+		self.usb = None
 
 	def connect(self, module_index):
 		if self.stem.connect(self.modules[module_index]) is not 0:
 			rospy.logerr('Usb hub %d connection failed...check your device' % module_index)
 		else:
 			rospy.loginfo('Usb hub %d connect success!' % module_index)
-		self.usb = bs.stem.USB(self.stem)
+		self.usb = bs.stem.USB(self.stem, 0)
+
+	def disconnect(self):
+		if self.stem.disconnect() is not 0:
+			rospy.logerr('Usb hub disconnection failed...check your device')
+		else:
+			rospy.loginfo('Usb hub disconnect success!')
 	
 	def enable(self, port_num):
 		if port_num < 0 or port_num > 7:
@@ -26,8 +55,10 @@ class usb_hub(Object):
 		else:
 			if self.usb.setPortEnable(port_num) is not 0:
 				rospy.logerr('port %d enabling failed...might be usb hub connection problem' % port_num)
+				return 0
 			else:
 				rospy.loginfo('port %d enabling success!' % port_num)
+				return 1
 
 	def disable(self, port_num):
 		if port_num < 0 or port_num > 7:
@@ -35,8 +66,10 @@ class usb_hub(Object):
 		else:
 			if self.usb.setPortDisable(port_num) is not 0:
 				rospy.logerr('port %d disabling failed...might be usb hub connection problem' % port_num)
+				return 0
 			else:
 				rospy.loginfo('port %d disabling success!' % port_num)
+				return 1
 
 	def enable_all(self):
 		for port in range(8):
@@ -47,11 +80,20 @@ class usb_hub(Object):
 			self.disable(port)
 
 	def get_state(self, port_num):
-		bank = port_num / 4
+		bank = port_num / 4  # bank 0: 0-3 bank 1: 4-7
 		index = port_num % 4
 		mask = 1 << (2*index)
 		if self.usb.getHubState(bank) & mask is not 0:
 			return 1    # port is working
 		else:
 			return 0    # port is not working
+
+	def find_hub_and_port(self, camera_serial):
+		for i in range(len(self.modules)):
+			try:
+				port = self.device_list[i].index(camera_serial)
+				return (i, port)
+			except:
+				continue
+		return -1
 
--- USB_HUB_SERVER.py
+++ USB_HUB_SERVER.py
@@ -1,1 +1,53 @@
+#!/usr/bin/env python
 '''
+THIS CLASS IS AN ENCAPSULATION OF USB HUB SERVER, ALSO AN INTERFACE BETWEEN ROS AND USB HUB SOFTWARE LAYER
+CREATED BY FUHENG DENG 5/31/2017
+'''
+
+import rospy
+from usb_hub_server.srv import *
+from std_msgs.msg import String
+import brainstem as bs
+from USB_HUB import *
+import numpy as np
+import time
+
+class usb_hub_server:
+
+	def __init__(self):
+		self.usb_hub = usb_hub()   # Only connect when necessary
+		rospy.Service('usb_hub_server', err_string, self.err_handler)
+	
+	def err_handler(self,req):
+		index = self.usb_hub.find_hub_and_port(req.serial_num)
+		if not isinstance(index, tuple):
+			rospy.logerr('Serial number doesn\'t exist in the list' % req.serial_num)
+			return err_stringResponse(req.serial_num + " 1")
+		# initialize the usb hub and get the usb instance
+		hub_index = index[0]
+		port_index = index[1] 	
+		self.usb_hub.connect(hub_index)
+		self.usb_hub.disable(port_index)	
+		e_r = self.usb_hub.enable(port_index)
+		try_num = 5   # try 5 times to ensure enabling
+		while e_r is not 0 and try_num > 0:
+			rospy.logwarn('usb_hub node: unable to enable the port...try again...')
+			e_r = self.usb_hub.enable(port_index)
+			time.sleep(0.2)
+			try_num -= 1
+		self.usb_hub.disconnect()
+		# return based on enabling result
+		if e_r is 0:
+			rospy.loginfo('Device(serial number:%s) has been successfully enabled' % req.serial_num) 
+			res = req.serial_num + " 0"   # example return: 16369133 0 - enabling success	
+			return err_stringResponse(res)
+		else:	
+			rospy.logerr('Device(serial number:%s) enabling failed' % req.serial_num)
+			res = req.serial_num + " 1"  # example return: 16369133 1 - enabling fail
+			return err_stringResponse(res)
+		
+
+if __name__ == "__main__":
+	rospy.init_node('USB_HUB_SERVER')
+	_ = usb_hub_server()
+	rospy.spin()
